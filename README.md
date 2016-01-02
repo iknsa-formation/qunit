c726a93aa239ce0f8a362291cda792c81155b461
Khalid Sookia <khalidsookia@gmail.com>
2016-01-02 15:44:31 +0100

    Added property equal for different objects with same values

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ There is a function nbCars which takes a number that should only be positve and 
 
 To test this function we will submit different +ve and -ve values to the function and see if it does return what is expected
 
-### Assert Equal
+### Assertions
 ```
 # tag 0.2.0
 git checkout 0.2.0
@@ -18,3 +18,11 @@ git checkout 0.2.0
 Here we are going to test some assertions. We'll keep it simple, is 1 = 1.
 
 As you can see there can be different ways to check if 1 is actually equal to 1 since there can be type checking also
+
+```
+# tag 0.2.1
+git checkout 0.2.1
+```
+Here we are going to test some assertions on different objects containing the same values.
+
+What we want to achieve is to check if 2 different objects contains the same properties and values.
--- tests/tests.js
+++ tests/tests.js
@@ -1,15 +1,48 @@
-test('nbCars()', function() {
+// http://qunitjs.com/
+
+// Assertions
+test('nunber of Cars', function() {
     ok(nbCars(0), 'Zero is a possible number of cars');
     ok(nbCars(2), 'Two is a possible number of cars');
     ok(!nbCars(-2), 'Negative two is NOT a possible number of cars');
 });
 
-test('assertions', function() {
+// Equal assertion
+test('equal', function() {
     // equal() uses ==
     equal(1,1);
     equal(1,1, 'One is really equal to one...');
     equal(1,'1', 'One is really equal to one...');
-    // deepEqual() uses ===
+    notEqual( {}, {}, 'Returns true as the two objects are different objects');
+    notEqual( [], [], 'Returns true as the two arrays are different arrays');
+
+    /**
+     * @fromTheDoc
+     *
+     * A deep recursive comparison, working on primitive types, arrays, objects, regular expressions, dates and functions.
+     *
+     * deepEqual() uses ===
+     */
     deepEqual(1,1, 'One is really equal to one...');
     notDeepEqual(1,'1', 'One is really equal to one...');
+    // strictlyEqual() uses ===
+    deepEqual(1,1, 'One is really equal to one...');
+    notDeepEqual(1,'1', 'One is really equal to one...');
+});
+
+// Identical assertion
+
+/**
+ * @fromTheDoc
+ *
+ * The propEqual() assertion provides strictly (===) comparison of Object properties. Unlike deepEqual(), this assertion can be
+ * used to compare two objects made with different constructors and prototype.
+ *
+ * strictEqual() can be used to test strict equality.
+ *
+ * notPropEqual() can be used to explicitly test strict inequality of Object properties
+ */
+test('propEqual', function() {
+    propEqual( {a: 1}, {a: 1} , 'Although the objects are different, there properties are even');
+    propEqual( [1], [1], 'Returns true event though the two arrays are different arrays');
 });
