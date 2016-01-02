e3d94c59cbba4efd3fabbdb5c731a5fe8f2c2023
Khalid Sookia <khalidsookia@gmail.com>
2016-01-02 13:58:49 +0100

    equal and deepEqual

--- tests/tests.js
+++ tests/tests.js
@@ -3,3 +3,13 @@ test('nbCars()', function() {
     ok(nbCars(2), 'Two is a possible number of cars');
     ok(!nbCars(-2), 'Negative two is NOT a possible number of cars');
 });
+
+test('assertions', function() {
+    // equal() uses ==
+    equal(1,1);
+    equal(1,1, 'One is really equal to one...');
+    equal(1,'1', 'One is really equal to one...');
+    // deepEqual() uses ===
+    deepEqual(1,1, 'One is really equal to one...');
+    notDeepEqual(1,'1', 'One is really equal to one...');
+});
